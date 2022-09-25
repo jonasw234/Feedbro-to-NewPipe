9e5b457eb13971993cfe2606982730a97d802ca9
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2022-09-25 17:28:49 +0000

    Create a new database file by default
    Newer NewPipe versions don’t have the import for CSV files anymore.

--- feedbro_to_newpipe.py
+++ feedbro_to_newpipe.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """Converts Feedbro YouTube subscriptions to NewPipe subscriptions."""
-import csv
+import os
+import pathlib
+import sqlite3
 import sys
+import tempfile
 import xml.etree.ElementTree as ET
+import zipfile
 from os.path import basename
 
 
@@ -25,21 +29,33 @@ def main(feedbro_input: str, newpipe_output: str):
         and channel.attrib["htmlUrl"].startswith("https://www.youtube.com/channel/")
     }
 
-    # Export to NewPipe csv file
-    with open(newpipe_output, "w", encoding="utf-8") as newpipe_subscriptions:
-        newpipe_csv = csv.writer(newpipe_subscriptions)
-        newpipe_csv.writerow(["Channel Id", "Channel Url", "Channel Title"])
-        for title, url in youtube_channels.items():
-            newpipe_csv.writerow(
-                [url[len("https://www.youtube.com/channel/") :], url, title]
-            )
+    # Export to NewPipe database file
+    with tempfile.TemporaryDirectory() as tempdir:
+        with zipfile.ZipFile("NewPipeData.zip") as inzip:
+            inzip.extractall(tempdir)
+            with sqlite3.connect(
+                os.path.join(tempdir, "newpipe.db"), isolation_level=None
+            ) as con:
+                cur = con.cursor()
+                cur.execute("PRAGMA journal_mode=DELETE;")
+                cur.execute("DELETE FROM subscriptions;")
+                for channel_name, channel_url in youtube_channels.items():
+                    cur.execute(
+                        'INSERT INTO subscriptions (service_id, url, name, avatar_url, subscriber_count, description) VALUES (0, ?, ?, "", 0, "");',
+                        (channel_url, channel_name),
+                    )
+                    con.commit()
+        with zipfile.ZipFile(newpipe_output, "w", zipfile.ZIP_DEFLATED) as outzip:
+            tempdir_path = pathlib.Path(tempdir)
+            for file_path in tempdir_path.rglob("*"):
+                outzip.write(file_path, arcname=file_path.relative_to(tempdir))
+
     print(f"Export completed and saved to {newpipe_output}.")
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print(
-            f"Usage: {basename(__file__)} Feedbro-Subscriptions.opml NewPipe-Subscriptions.csv"
-        )
+        print(f"Usage: {basename(__file__)} Feedbro-Subscriptions.opml NewPipeData.zip")
         sys.exit(1)
-    main(*sys.argv[1:])
+    else:
+        main(*sys.argv[1:])
